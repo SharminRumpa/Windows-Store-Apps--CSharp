371581fa77ec571434adf378062f9cf3a4a86910
Sharmin Rumpa <sharminakterrumpa@gmail.com>
2020-10-08 22:40:17 +0600

    Initial commit

--- SQLQuery-Bookdb.sql
+++ SQLQuery-Bookdb.sql
@@ -1,3 +1,6 @@
+
+/******Create  Database for Windows Store App***********************/
+
 GO
 USE master;
 GO
@@ -10,6 +13,8 @@ CREATE DATABASE BooksBD;
 GO
 USE BooksBD;
 
+/***********************Create Books Table***********************/
+Go
 create TABLE Books (	BookID				INT				PRIMARY KEY,
 						BookTitle			VARCHAR(100),
 						AuthorName			VARCHAR(50),
@@ -17,13 +22,22 @@ create TABLE Books (	BookID				INT				PRIMARY KEY,
 						NumberOfPage		INT,
 						PublicationName		VARCHAR(50),
 						PublishYear			INT);
+					
+					
+/***********************Insert into Books Table***********************/
 
-
+Go
 INSERT INTO Books	 VALUES			(1, 'The Bluest Eye', 'William Shakespere', 'O-671-854216', 230, 'Lyall Publication', 1965),	
 									(2,'Mrityukshuda',   'Rabindranath Tagor', 'O-688-1433-4', 150,'Agamee Prakshani', 1890), 
 									(3,'She Stoop', 'Joan Magretta', 'O-525-67520-5', 290, 'Clarlon Books', 1949),
 									(4,'Agni Bani', 'Kazi Nazrul Islam', 'O-14-0125867', 290, 'Somoy Prokashon', 1922),
-									(5,'Ratri Shesh', 'Ahsan Habib', 'O-14-11205867',300, 'Kakoli Prokashoni', 1930);	
+									(5,'Ratri Shesh', 'Ahsan Habib', 'O-14-11205867',300, 'Kakoli Prokashoni', 1930);
+
+
+/**********************1252366 - Sharmin Akter************************************/
+
+Go
+Select * from Books;									
 
 
 									
